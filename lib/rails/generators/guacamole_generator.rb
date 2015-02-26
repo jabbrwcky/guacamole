e2bd676107fd51a3b1a6b157a3fb2d58dba5d002
Dirk Breuer <dirk.breuer@gmail.com>
2015-02-26 14:28:24 +0100

    Generator: The scaffold controller will have working code

--- lib/rails/generators/guacamole_generator.rb
+++ lib/rails/generators/guacamole_generator.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 
 require 'rails/generators/named_base'
-require 'rails/generators/active_model'
+require 'rails/generators/guacamole/active_model'
 
 module Guacamole
   module Generators
@@ -22,6 +22,14 @@ module Rails
         return 'String' if type == :text
         return 'Boolean' if type == :boolean
         type.to_s.camelcase
+      end
+    end
+
+    class ScaffoldControllerGenerator < NamedBase # :nodoc:
+      # We need to inject our own controller template because the original template
+      # will not work with the data mapper approach used by Guacamole.
+      def source_paths
+        [File.join(File.dirname(__FILE__), 'rails', 'scaffold_controller', 'templates')]
       end
     end
   end
