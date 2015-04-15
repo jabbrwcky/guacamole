154a371811e356015374bfbe10b2c674938338ea
Jens Hausherr <jens.hausherr@xing.com>
2015-04-15 12:03:10 +0200

    add todo as marker for cleaning up invese relations

--- lib/guacamole/proxies/hash.rb
+++ lib/guacamole/proxies/hash.rb
@@ -11,7 +11,7 @@ module Guacamole
         if relates_to_collection?
           query_result.map{ |e| [e.edge_attributes['hash_key'], e.model] }.to_h
         else
-          ::Kernel.puts "Hash-nocollection #{query_result}"
+          # todo change to Guacamole::Proxies::Single for inverse relations
           query_result.first.model
         end
       end
